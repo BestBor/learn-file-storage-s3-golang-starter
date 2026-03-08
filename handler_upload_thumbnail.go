c83fa507699c9cd4bdfc1f7d6e085fd6b67b463e
BestBor <185581264+BestBor@users.noreply.github.com>
2026-03-07 23:05:31 -0500

    completed upload thumbnail function

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
+)
+
+const (
+	maxMemory int64 = 10 << 20
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
@@ -28,10 +34,52 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "couldn't parse multipart form", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	fileData, headers, err := r.FormFile("thumbnail") // html input name
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer fileData.Close()
+
+	mediaType := headers.Header.Get("Content-Type")
+
+	imgData, err := io.ReadAll(fileData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't read image data", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to retrieve video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "unauthorized", errors.New("request made by unauthorized user"))
+		return
+	}
+
+	videoThumbnails[video.ID] = thumbnail{
+		data:      imgData,
+		mediaType: mediaType,
+	}
+	newURL := fmt.Sprintf("http://localhost:%v/api/thumbnails/%s", cfg.port, videoIDString)
+	video.ThumbnailURL = &newURL
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "internal server error", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
